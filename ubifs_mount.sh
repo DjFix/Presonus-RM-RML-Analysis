7041dc6c106856ea5c0eec18297dd6135163a303
Roman <romansavrulin@gmail.com>
2019-07-10 18:07:26 +0300

    added scripts folder

--- ubifs_mount.sh
+++ ubifs_mount.sh
@@ -10,6 +10,6 @@ rmmod ubi
 rmmod mtdram
 modprobe mtdram total_size=25600 erase_size=128
 flash_erase /dev/mtd0 0 0
-ubiformat /dev/mtd0  -f $SOURCE_IMG -v -O 64 -e 1
+ubiformat /dev/mtd0  -f $SOURCE_IMG -O 64 -e 1
 modprobe ubi mtd=0
 mount -t ubifs /dev/ubi0_0 $TARGET_DIR
