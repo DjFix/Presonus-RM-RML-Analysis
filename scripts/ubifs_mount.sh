3ebad38c56ab8e99bd9aa7d3207b7c5e28c4da52
root <root@gitlab.flexibity.com>
2019-07-13 21:17:02 +0300

    add rootfs generation script

--- scripts/ubifs_mount.sh
+++ scripts/ubifs_mount.sh
@@ -2,14 +2,14 @@
 set -e
 
 TARGET_DIR=/media/cdrom
-SOURCE_IMG=rootfs.img
+SOURCE_IMG=$1
 
 umount $TARGET_DIR 2>/dev/null || true
-rmmod ubifs
-rmmod ubi
-rmmod mtdram
+rmmod ubifs || true
+rmmod ubi || true
+rmmod mtdram || true
 modprobe mtdram total_size=25600 erase_size=128
 flash_erase /dev/mtd0 0 0
-ubiformat /dev/mtd0  -f $SOURCE_IMG -O 64 -e 1
+ubiformat /dev/mtd0 -f $SOURCE_IMG -O 64 -e 1 -s 1
 modprobe ubi mtd=0
 mount -t ubifs /dev/ubi0_0 $TARGET_DIR
